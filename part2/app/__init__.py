b6de331144755f94c8edbd94e1cf3d3f80592a6e
Bruno Dos Santos <brudossant@Brunos-MacBook-Air.local>
2025-03-02 02:45:27 -0300

    implementacion de facade y api de place completa

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -8,8 +8,9 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Register the users namespace
+    # Register the namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path="/places")
+    api.add_namespace(places_ns, path='/api/v1/places')
+    
     return app
